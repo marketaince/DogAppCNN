366ff63758d0b28995ac04927d4cd17cc913d88c
Markéta Ince <marketa.ince@gmail.com>
2020-08-31 00:01:23 +0200

    Reformatting flask app

--- DogAppDeploy/main.py
+++ DogAppDeploy/main.py
@@ -1,17 +1,37 @@
+"""
+Flask App For Dog App Predictions
+"""
+
+# ------- #
+# Imports #
+# ------- #
+
 from flask import Flask
 from flask_restful import Api
-
-from lambda_function import PredictImage
 from flask_cors import CORS, logging
 
+from predict_image_resource import PredictImage
+
+# -------------- #
+# App Definition #
+# -------------- #
+
+# Create new Flask instance
 app = Flask(__name__)
 
+# Settings
 app.config["PROPAGATE_EXCEPTIONS"] = True
 cors = CORS(app)
 logging.getLogger('flask_cors').level = logging.DEBUG
 api = Api(app)
 
+# Adding Resources
 api.add_resource(PredictImage, '/predict')
+
+
+# ------- #
+# Run App #
+# ------- #
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, threaded=False)
